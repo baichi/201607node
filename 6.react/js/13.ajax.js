001ab2aeb3072f05bc7f4d08e5170e5ce01e4d7f
zhufengnodejs <zhufengnodejs@126.com>
2016-10-06 17:22:21 +0800

    ajax

--- 6.react/js/13.ajax.js
+++ 6.react/js/13.ajax.js
@@ -1,5 +1,19 @@
+/**
+ * 1.为input绑定onChange事件，当事件发生的时候执行处理函数
+ * 2. 在处理函数中，先得到input的值，然后调用ajax方法来获取百度返回的数据
+ * 3. 修改组件的words状态,等于一个li数组，从而刷新视图
+ */
 var Suggest = React.createClass({
+    getInitialState(){
+      return {words:[]}
+    },
     componentDidMount(){
+        /**
+         * 解决success里的this指针
+         * 1. 缓存this
+         * 2. 箭头函数
+         * 3. 指定 context
+         */
         $.ajax({
             url:'http://www.baidu.com/su',//接口
             method:'GET',//请求的方法
@@ -19,7 +33,7 @@ var Suggest = React.createClass({
                 </div>
                 <div className="panel-body">
                     <ul className="list-group">
-
+                        {this.state.words}
                     </ul>
                 </div>
             </div>
