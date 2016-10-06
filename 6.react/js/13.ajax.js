73ac1621ebf1b61ce420f71541d3c70372e196cc
zhufengnodejs <zhufengnodejs@126.com>
2016-10-06 17:37:20 +0800

    ajax

--- 6.react/js/13.ajax.js
+++ 6.react/js/13.ajax.js
@@ -7,21 +7,24 @@ var Suggest = React.createClass({
     getInitialState(){
       return {words:[]}
     },
-    componentDidMount(){
+    handleChange(event){
         /**
          * 解决success里的this指针
          * 1. 缓存this
          * 2. 箭头函数
          * 3. 指定 context
          */
+        var val = event.target.value;
         $.ajax({
             url:'http://www.baidu.com/su',//接口
             method:'GET',//请求的方法
             jsonp:'cb',//用来传送返回方法名的参数名
             dataType:'jsonp',//指定返回值的类型
-            data:{wd:'a'},//要传输的参数
+            data:{wd:val},//要传输的参数
+            context:this,
             success:function(result){
-                console.log(result);
+                var words = result.s.map((item,index)=><li className="list-group-item" key={index}>{item}</li>);
+                this.setState({words});
             }
         })
     },
@@ -29,7 +32,7 @@ var Suggest = React.createClass({
         return (
             <div className="panel panel-primary">
                 <div className="panel-heading">
-                    <input type="text" className="form-control"/>
+                    <input onChange={this.handleChange} type="text" className="form-control"/>
                 </div>
                 <div className="panel-body">
                     <ul className="list-group">
