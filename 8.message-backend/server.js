5dd06b4d5016d81055eff167151d3216864e9c34
zhufengnodejs <zhufengnodejs@126.com>
2016-10-07 13:11:01 +0800

    实现后台接口

--- 8.message-backend/server.js
+++ 8.message-backend/server.js
@@ -20,6 +20,17 @@ Message.create([
 var app = express();
 //解析json格式的请求体 把请求体对象放在req.body上
 app.use(bodyParser.json());
+app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin','*');//允许访问的来源
+    res.setHeader('Access-Control-Allow-Headers','Content-Type');//允许发哪些请求头
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,DELETE,OPTIONS');
+    // preflight是询问服务器是否可以继续请求
+    if(req.method == 'OPTIONS'){
+        res.send('');
+    }else{
+        next();
+    }
+});
 // messages 增加消息 查看消息 删除消息 postman
 app.post('/messages',function(req,res){
     var message = req.body;
