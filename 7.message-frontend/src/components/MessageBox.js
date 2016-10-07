92d76327bf6cf82c8a80bae44d4a5368daa2115f
zhufengnodejs <zhufengnodejs@126.com>
2016-10-07 13:07:16 +0800

    实现后台接口

--- 7.message-frontend/src/components/MessageBox.js
+++ 7.message-frontend/src/components/MessageBox.js
@@ -1,10 +1,25 @@
 /**
- * es 声明组件方法
+ * 1. 在constructor里调用init
+ * 2. init方法里调用后台接口获取消息的数组
+ * 3. 修改当前组件状态对象的messages属性 setState()
+ * 4. 把状态传递通过属性传递给MessageList组件
+ * 5. 在MessageList组件中先得到属性中的messages，然后把这个JSOn数组转成li数组显示即可。
  */
 import React from 'react';
 import MessageList from './MessageList';
 import MessageForm from './MessageForm';
 export default class MessageBox extends React.Component {
+    constructor(props){
+        super(props);
+        //这是es6里定义初始状态的方法
+        this.state = {messages:[]};
+        this.init();
+    }
+    //调用ajax接口获取数据,
+    init(){
+
+    }
+
     render() {
         return (
             <div className="container">
