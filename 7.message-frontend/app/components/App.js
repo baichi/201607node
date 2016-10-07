41b936eabb06115ebe08a306305f43ca5147df12
zhufengnodejs <zhufengnodejs@126.com>
2016-10-07 17:30:08 +0800

    实现导航

--- 7.message-frontend/app/components/App.js
+++ 7.message-frontend/app/components/App.js
@@ -3,7 +3,26 @@ export default class App extends React.Component{
     render(){
         return (
             <div className="container">
-                React路由
+               <nav className="navbar navbar-inverse">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <div className="navbar-brand">珠峰React</div>
+                        </div>
+                        <div>
+                            <ul className="nav navbar-nav">
+                                <li>
+                                    <a href="#">首页</a>
+                                </li>
+                                <li>
+                                    <a href="#">用户管理</a>
+                                </li>
+                                <li>
+                                    <a href="#">个人设置</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+               </nav>
             </div>
         )
     }
