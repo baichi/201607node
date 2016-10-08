5b03b3b7d658a190f1897d9e567e2806b03884fd
zhufengnodejs <zhufengnodejs@126.com>
2016-10-08 15:59:01 +0800

    socket

--- 10.socketio/server.js
+++ 10.socketio/server.js
@@ -12,14 +12,21 @@ var server = require('http').createServer(app);
 //socket.io依赖于http服务
 var io = require('socket.io')(server);
 //监听客户端连接事件,连接到来的时候会为每个客户端创建一个socket对象，传入函数内执行
+var messages  = [];
 io.on('connection',function(socket){
     //socket.send('客户端你好');
     //socket.emit('message','客户端你好');
    //监听客户端发过来的消息
    socket.on('message',function(msgObj){
        //把收到的某个人发给服务器的消息对象发给所有的人
+       console.log(msgObj);
+       messages.push(msgObj);
       io.emit('message',msgObj);
    });
+    //监听客户端需要所有消息的事件
+    socket.on('getAllMessages',function(){
+      socket.emit('allMessages',messages);
+    });
 });
 server.listen(8080,function(){
     console.log('服务器已经在8080端口监听成功');
